e2fc7e63b99a4b9561b352ab1564ef9bade6e371
Kevin Levitz <tug97195@temple.edu>
2020-09-20 17:34:42 -0400

    Created logic to check if conditions are met for project requirements

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -2,7 +2,14 @@ package edu.temple.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -11,13 +18,43 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                TextView textView = (TextView)findViewById(R.id.textView);
-//                textView.setText("You have clicked the button");
-//            }
-//
-//        });
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                //Check if all imports are given
+                EditText editName = (EditText) findViewById(R.id.editName);
+                EditText editEmail = (EditText) findViewById(R.id.editEmail);
+                EditText editPassword = (EditText) findViewById(R.id.editPassword);
+                EditText editPassword2 = (EditText) findViewById(R.id.editPassword2);
+                String name = editName.getText().toString();
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+                String password2 = editPassword2.getText().toString();
+
+                if(name.equals("") || email.equals("") || password.equals("") || password2.equals("")){
+                    Context context = getApplicationContext();
+                    CharSequence text = "ERROR: You did not complete the form !!";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else{
+                    if(password.equals(password2)){
+                        Context context = getApplicationContext();
+                        CharSequence text = "Welcome: " + name;
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    } else{
+                        Context context = getApplicationContext();
+                        CharSequence text = "ERROR: Passwords do not match !! Given: " + password + " and " + password2;
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+
+                }
+            }
+        });
     }
 }
